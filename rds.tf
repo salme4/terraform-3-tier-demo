ad50ab08fed35c5df2d7c6f2f7d6efc855a56047
김대현Daehyun kim <daehyun.kim@bespinglobal.com>
2021-09-06 15:50:42 +0900

    rds & webserver 주석

--- rds.tf
+++ rds.tf
@@ -1,22 +1,22 @@
-resource "aws_db_instance" "tier-db-instance" {
-  allocated_storage      = 10
-  db_subnet_group_name   = aws_db_subnet_group.default.id
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
-  multi_az               = true
-  name                   = "demodb"
-  username               = var.db_username
-  password               = var.db_password
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.web-db-sg.id]
-}
-
-resource "aws_db_subnet_group" "default" {
-  name       = "tier-db-subnet-group"
-  subnet_ids = [aws_subnet.tier-db-subnet-1.id, aws_subnet.tier-db-subnet-2.id]
-
-  tags = {
-    Name = "${var.resource_prefix}-DB-SUBNET-GROUP"
-  }
-}
+//resource "aws_db_instance" "tier-db-instance" {
+//  allocated_storage      = 10
+//  db_subnet_group_name   = aws_db_subnet_group.default.id
+//  engine                 = "mysql"
+//  engine_version         = "5.7"
+//  instance_class         = "db.t2.micro"
+//  multi_az               = true
+//  name                   = "demodb"
+//  username               = var.db_username
+//  password               = var.db_password
+//  skip_final_snapshot    = true
+//  vpc_security_group_ids = [aws_security_group.web-db-sg.id]
+//}
+//
+//resource "aws_db_subnet_group" "default" {
+//  name       = "tier-db-subnet-group"
+//  subnet_ids = [aws_subnet.tier-db-subnet-1.id, aws_subnet.tier-db-subnet-2.id]
+//
+//  tags = {
+//    Name = "${var.resource_prefix}-DB-SUBNET-GROUP"
+//  }
+//}
